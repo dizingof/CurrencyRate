d6414cd3012c82260971dcd88c78005890ea9d1c
DESKTOP-KUSCP3I\roman <romannep1989@gmail.com>
2024-09-03 21:41:27 +0300

    change schedule

--- CurrencyRate.WebApi/Program.cs
+++ CurrencyRate.WebApi/Program.cs
@@ -59,7 +59,7 @@ try
     // 7. Settings scheduler
     app.Services.UseScheduler(scheduler =>
     {
-        scheduler.Schedule<CurrencyRateJob>().EveryFifteenMinutes();
+        scheduler.Schedule<CurrencyRateJob>().EveryFiveMinutes();
     });
 
     // 8. Setting differents midlware
